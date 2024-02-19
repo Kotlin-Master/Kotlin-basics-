cfa4f9195bf2450c9b9081327e8b800a26bb40a4
Syed Mustafa Hassan <98483655+Mustafa-Hassan2001@users.noreply.github.com>
2024-02-19 23:15:40 +0500

    Create lecture2.kt

---
+++ lecture2.kt
@@ -0,0 +1,21 @@
+fun main() {
+  println("Hello World!")
+  println("I am learning Kotlin.")
+  println("It is awesome!")
+  println(3 + 3)
+// This is a comment
+println("Hello World") 
+/* The code below will print the words Hello World
+to the screen, and it is amazing */
+println("Hello World")  
+var name = "John"
+val birthyear = 1975
+
+println(name)          // Print the value of name
+println(birthyear)     // Print the value of birthyear
+var name = "John"      // String (text)
+val birthyear = 1975   // Int (number)
+
+println(name)          // Print the value of name
+println(birthyear)     // Print the value of birthyear
+}
